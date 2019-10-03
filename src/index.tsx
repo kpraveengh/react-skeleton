6ac681103d48ed91d503c44707ddcae2f4af75e9
Praveen Kumar <praveenkumar@PTIMAC71.local>
2019-10-03 19:05:08 +0530

    react skeleton for app development

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import React, { createElement } from "react";
+import ReactDOM, { render } from "react-dom";
+
+import App from './App'
+
+
+
+// Render the Root element into the DOM
+render(
+  <App />,
+  document.getElementById('root'),
+);
