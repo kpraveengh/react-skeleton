82e9e956aad9814fd4620fcc596c652a882bd3c9
Praveen Kumar <praveenkmr841@gmail.com>
2019-10-12 13:04:43 +0530

    createelement  issue fixed with tsconfig update

--- src/_components/_layouts/sidebar.layout.tsx
+++ src/_components/_layouts/sidebar.layout.tsx
@@ -166,47 +166,6 @@ class AppSidebar extends Component<any, any>{
                 </a>
               </div>
             </nav>
-            <main className="page-content">
-              <div className="container-fluid">
-                <h2>Pro Sidebar</h2>
-                <hr />
-                <div className="row">
-                  <div className="form-group col-md-12">
-                    <p>This is a responsive sidebar template with dropdown menu based on bootstrap 4 framework.</p>
-                    <p> You can find the complete code on <a href="https://github.com/azouaoui-med/pro-sidebar-template" target="_blank">
-                        Github</a>, it contains more themes and background image option</p>
-                  </div>
-                  <div className="form-group col-md-12">
-
-                  </div>
-                </div>
-                <h5>More templates</h5>
-                <hr />
-                <div className="row">
-                  <div className="col-xs-12 col-sm-6 col-md-6 col-lg-4">          
-                    <div className="card rounded-0 p-0 shadow-sm">
-                      <img src="https://user-images.githubusercontent.com/25878302/58369568-a49b2480-7efc-11e9-9ca9-2be44afacda1.png" className="card-img-top rounded-0" alt="Angular pro sidebar" />
-                      <div className="card-body text-center">
-                          <h6 className="card-title">Angular Pro Sidebar</h6>
-                          <a href="https://github.com/azouaoui-med/angular-pro-sidebar" target="_blank" className="btn btn-primary btn-sm">Github</a>
-                          <a href="https://azouaoui-med.github.io/angular-pro-sidebar/demo/" target="_blank" className="btn btn-success btn-sm">Preview</a>
-                      </div>
-                    </div>          
-                  </div>
-                          <div className="col-xs-12 col-sm-6 col-md-6 col-lg-4">          
-                    <div className="card rounded-0 p-0 shadow-sm">
-                      <img src="https://user-images.githubusercontent.com/25878302/58369258-33f20900-7ef8-11e9-8ff3-b277cb7ed7b4.PNG" className="card-img-top rounded-0" alt="Angular pro sidebar" />
-                      <div className="card-body text-center">
-                          <h6 className="card-title">Angular Dashboard</h6>
-                          <a href="https://github.com/azouaoui-med/lightning-admin-angular" target="_blank" className="btn btn-primary btn-sm">Github</a>
-                          <a href="https://azouaoui-med.github.io/lightning-admin-angular/demo/" target="_blank" className="btn btn-success btn-sm">Preview</a>
-                      </div>
-                    </div>          
-                  </div>
-                </div>
-                </div>
-
-            </main>
           </div>
         )}
 }
